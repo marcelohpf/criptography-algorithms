287238485bb817edbeda35c4d0ebb5cb7456e507
Marcelo Ferreira <marcelohpf@gmail.com>
2016-07-07 13:29:19 -0300

    Add file management to rsa
    Signed-off-by: Marcelo Ferreira <marcelohpf@gmail.com>

--- lista4/l4e1.py
+++ lista4/l4e1.py
@@ -21,6 +21,7 @@ def input_prime():
         p = input("Number is not prime, digit other: ")
 
     return p
+
 def input_pqe():
     """Make the read of values for calculate number n, and public key
         
@@ -40,17 +41,38 @@ def input_pqe():
     d %= fi_n # Transform in positive if d is negative
     return (p*q,e,d)
 
+def cipher(data,values):
+    output = []
+    for x in data:
+        # x^Pubkey mod n
+        cipher_data = pow(ord(x), values[PUBLIC_KEY],values[N_VALUE])
+        output.append(cipher_data)
+    return output
+
+def decipher(data,values):
+    output = []
+    for x in data:
+        # x^PrivKey mod n
+        decipher_data = pow(ord(x),values[PRIVATE_KEY],values[N_VALUE])
+        output.append(decipher_data)
+    return output
+
 if __name__ == '__main__':
     values = input_pqe()
-    name = input("name file with the data")
+    name = input("name file with the data\n")
     data = open(name,"r").read()
-
-    # write direct in the file the encryption
-    print("encrypting and writte in %s.crsa" % name)
-    output = [pow(ord(x),values[PUBLIC_KEY],values[N_VALUE]) for x in data]
-    open(name+".crsa","w").write("".join([chr(x) for x in output]))
-
-    # write decifred text direct in file the decryption
-    print("decrypting and writte in %s.drsa" % name)
-    output = [pow(x,values[PRIVATE_KEY],values[N_VALUE]) for x in output]
-    open(name+".drsa","w").write("".join([chr(x) for x in output]))
+    option = input("Select \ne - encrypt\nd - decrypt\n")
+    if option == 'e':
+        # write direct in the file the encryption
+        output = cipher(data,values)
+        print("encrypting and writte in %s.crsa" % name)
+        with open(name+".crsa","w") as file_cipher:
+            file_cipher.write("".join([chr(x) for x in output]))
+    elif option == 'd':
+        # write decifred text direct in file the decryption
+        print("decrypting and writte in %s.drsa" % name)
+        output = decipher(data,values)
+        with open(name+".drsa","w") as file_decipher:
+            file_decipher.write("".join([chr(x) for x in output]))
+    else:
+        print("Invalid option")
